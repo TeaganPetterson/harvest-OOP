a43e82aa2bf0b4765866837d9570a7333eedd324
Teagan Petterson <teagan.petterson1@gmail.com>
2023-05-04 16:50:28 -0600

    Somethings going wrong in the Get_sellability function, but idk what

--- harvest.py
+++ harvest.py
@@ -70,7 +70,7 @@ def print_pairing_info(melon_types):
         for pairing in melon.pairings:
             print(f"-{pairing}")
 
-print_pairing_info(melons)
+# print_pairing_info(melons)
 
 def make_melon_type_lookup(melon_types):
     """Takes a list of MelonTypes and returns a dictionary of melon type by code."""
@@ -83,7 +83,9 @@ def make_melon_type_lookup(melon_types):
 
     return melons_by_id
 
-melons_by_id = make_melon_type_lookup(melons)
+# melons_by_id = make_melon_type_lookup(melons)
+
+
 ############
 # Part 2   #
 ############
@@ -94,15 +96,47 @@ class Melon:
 
     # Fill in the rest
     # Needs __init__ and is_sellable methods
+    def __init__ (
+            self, melon_type, shape_rating, color_rating, field_number, harvester
+    ):
+        self.melon_type = melon_type
+        self.shape_rating = shape_rating
+        self.color_rating = color_rating
+        self.field_number = field_number
+        self.harvester = harvester
+
+        def is_sellable(self):
+            if self.shape_rating > 5 and self.color_rating > 5 and self.field_number != 3:
+                return True
+            else:
+                return False
 
 
 def make_melons(melon_types):
     """Returns a list of Melon objects."""
 
     # Fill in the rest
-
+    melons_by_id = make_melon_type_lookup(melon_types)
+    melon_1 = Melon(melons_by_id['yw'], 8, 7, 2, 'Sheila')
+    melon_2 = Melon(melons_by_id['yw'], 3, 4, 2, 'Sheila')
+    melon_3 = Melon(melons_by_id['yw'], 9, 8, 3, 'Sheila')
+    melon_4 = Melon(melons_by_id['cas'], 10, 6, 35, 'Sheila')
+    melon_5 = Melon(melons_by_id['cren'], 8, 9, 35, 'Michael')
+    melon_6 = Melon(melons_by_id['cren'], 8, 2, 35, 'Michael')
+    melon_7 = Melon(melons_by_id['cren'], 2, 3, 4, 'Michael')
+    melon_8 = Melon(melons_by_id['musk'], 6, 7, 4, 'Michael')
+    melon_9 = Melon(melons_by_id['yw'], 7, 10, 3, 'Sheila')
+    harvested_melons = [melon_1, melon_2, melon_3, melon_4, melon_5, melon_6, melon_7, melon_8, melon_9]
+    return harvested_melons
 
 def get_sellability_report(melons):
     """Given a list of melon object, prints whether each one is sellable."""
 
     # Fill in the rest
+    for melon in melons:
+        if melon.is_sellable() == True:
+            print(f"Harvested by {melon.harvester} from field {melon.field_number} (CAN BE SOLD)")
+        else:
+            print(f"Harvested by {melon.harvester} from field {melon.field_number} (CANNOT BE SOLD)")
+
+get_sellability_report(make_melons(melons))
